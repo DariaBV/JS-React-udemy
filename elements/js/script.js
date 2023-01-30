92007a00edf6ca9f1bc684bd20d617630a3ee360
DariaBV <dreamhappy8@gmail.com>
2023-01-30 17:52:30 +0100

    Added counters, elements, tasks and Food-dist

---
+++ elements/js/script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const box = document.getElementById('box'),
+      btns = document.getElementsByTagName('button'),
+      circles = document.getElementsByClassName('circle'),
+      hearts = document.querySelectorAll('heart'),
+      oneHeart = document.querySelector('.heart');
+
+// box.style.backgroundColor = 'blue';
+// box.style.width = '500px';
+
+box.style.cssText = 'background-color: blue; width: 500px';
+// box.style.cssText = `background-color: blue; width: ${num}px`;
+
+btns[1].style.borderRadius = '100%';
+circles[0].style.backgroundColor = 'red';  
+
+// for (let i = 0; i < hearts.length; i++) {
+//     hearts[i].style.backgroundColor = 'blue';
+// } 
+
+hearts.forEach(item => {
+    item.style.backgroundColor = 'blue'; 
+})
